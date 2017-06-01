ebcd167390216084252947827b9f92f00528e455
velascoDev <dracontium2@gmail.com>
2017-06-01 17:55:58 -0500

    Implemented basic project structure

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+require("./routes/get/views")(app);
+
+app.listen(port, () => {
+  console.log("Web server listening on port " + port);
+});
